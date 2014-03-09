0fa64124d3b47867387e3a6e75475622ebf0d22e
Patrick Daly <patrick@developdaly.com>
2014-03-08 19:16:32 -0600

    Better mobile styles

--- templates/home.php
+++ templates/home.php
@@ -13,7 +13,7 @@ get_header(); ?>
 			<div class="section-separators">
 				<section class="no-border-bg left-pull-visual top-border">
 					<div class="grid">
-						<div class="center-vertically" style="margin-top: 111px;">
+						<div class="center-vertically">
 							<h3>NerveTask, like WordPress, is open-source which means that everyone can make it better all the time.</h3>
 							<p class="large-type">Open-source projects benefit from their community of users because their users can help to actually build the product and add features as seen necessary to project management.</p>
 							<p class="light-type">
@@ -27,14 +27,14 @@ get_header(); ?>
 				</section>
 				<section class="right-pull-visual">
 					<div class="grid">
-						<div class="center-vertically" style="margin-top: 125px;">
+						<div class="center-vertically">
 							<h3>Your data is your data.</h3>
 							<p class="large-type">Project management data is important and should you ever choose to use a another platform you can take your data with you using a native WordPress export.</p>
 							<p class="light-type">
 								Never locked in. <a class="navigational" href="#">Learn more</a>
 							</p>
 						</div>
-						<div style="background: url('http://placehold.it/1613x775')" class="pull"></div>
+						<div style="background: url('http://placehold.it/900x573')" class="pull"></div>
 					</div>
 				</section>
 				<section class="single-testimonial facebook color-subtle_bg centered narrow">
@@ -42,7 +42,7 @@ get_header(); ?>
 				<section class="left-visual flush-both top-border">
 					<div class="grid">
 						<img src="http://placehold.it/500x495">
-						<div class="center-vertically" style="margin-top: 38px;">
+						<div class="center-vertically">
 							<h3>Prefer a different look? Skin NerveTask however you want.</h3>
 							<p class="large-type">NerveTask is an open-source plugin and easy for WordPress theme developers to create unique themes around. Or you can outfit an existing theme with a few steps.</p>
 							<p class="light-type">
@@ -56,7 +56,7 @@ get_header(); ?>
 				<section class="right-visual flush-bottom">
 					<div class="grid">
 						<img src="http://placehold.it/500x574">
-						<div class="center-vertically" style="margin-top: 129px;">
+						<div class="center-vertically">
 							<h3>Secure. NerveTask is built with WordPress VIP coding standards.</h3>
 							<p class="large-type">Quality code is no after-thought here. Security is important to your and your clients, so we've adhered to standards set forth by WordPress VIP, an Automattic company for enterprises.</p>
 							<p class="light-type">
