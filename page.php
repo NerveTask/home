750ca8dca03905f0c67aa81e91b876cf508a030e
Patrick Daly <patrick@developdaly.com>
2014-03-22 00:32:22 -0500

    Bootstrapped page template

--- page.php
+++ page.php
@@ -12,23 +12,33 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-lg-8">
-		<main id="main" class="site-main grid" role="main">
+<div class="container">
+		
+	<div class="row">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<section id="primary" class="content-area col-lg-12">
+			
+			<main id="main" class="site-main grid" role="main">
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || '0' != get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+			
+		</section><!-- #primary -->
+		
+	</div><!-- .row -->
+	
+</div><!-- .container -->
 
 <?php get_footer(); ?>
